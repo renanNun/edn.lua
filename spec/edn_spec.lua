7871a9d4ef00e6d6ef8fede05b7da301817fa2b6
renanNun <renannunes400@gmail.com>
2025-07-15 21:17:27 -0300

    feat: improve numbers values

--- spec/edn_spec.lua
+++ spec/edn_spec.lua
@@ -21,4 +21,21 @@ describe("EDN Decode", function()
         local result = edn.decode('"{}[]():;#"')
         assert.are.equal("{}[]():;#", result)
     end)
+
+    it("should parse negative values correctly", function()
+        local result = edn.decode('"-25.00M"')
+        assert.are.equal("-25.00M", result)
+    end)
+
+    it("should parse strign with special characters and numbers", function()
+        local result = edn.decode('":amount 30M"')
+        assert.are.equal(":amount 30M", result)
+    end)
+
+    it("should parse strign with special characters and numbers with more complex values", function()
+        local result = edn.decode('"{:amount 30M :total-amount 89.5679M}"')
+        assert.are.equal("{:amount 30M :total-amount 89.5679M}", result)
+    end)
+
+    
 end)
